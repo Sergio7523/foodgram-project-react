2f628743653796352c66fe8ea042c2c3e16b5216
Sergio7523 <orlovsergey263759223@gmail.com>
2023-04-04 19:28:04 +0300

    v1.15

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -193,7 +193,7 @@ class Favorite(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='favorite_recipe',
+        related_name='favorites',
         verbose_name='Рецепт',
     )
 
